cf4c40d076a3107b854c264b4b89929d142f1f2c
Rui Gu <rui.gu3@gmail.com>
2015-12-12 18:09:26 -0500

    Add script to parse valgrind output valgrindOutputParser.py will filter out the unused information from the original race report and generate the format will need.

---
+++ scripts/valgrindOutputParser.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python2.7
+import argparse
+import re
+import sys
+
+'''
+This python script transfers valgrind race report to the format required by
+our LLVM pass.
+A typical valgrind race report line
+==7832==    at 0x8068BB7: ap_buffered_log_writer (mod_log_config.c:1345)
+will be changed to
+ap_buffered_log_writer (mod_log_config.c:1345)
+'''
+
+# All the defined regular expressions
+regCallStackLine = re.compile("==[0-9]*==[\s]*(at|by) "
+        "[0-9A-Fx]*: ([a-zA-Z0-9_]*) (\([a-zA-Z0-9_\.]*:[0-9]*\))")
+regLineBreak = re.compile("==[0-9]*==[\s]*$")
+regReportStart = re.compile("==[0-9]*== (Possible data race during read|"
+        "This conflicts with a previous read)")
+
+def main(args):
+    try:
+        fin = open(args.raceReportIn, "r")
+    except IOError:
+        sys.stderr.write('Error: Input file does not exist!\n')
+        exit(1)
+    fout = open(args.raceReportOut, "w")
+    ifReportStart = False
+    for line in fin:
+        callStackLine = regCallStackLine.match(line)
+        lineBreak = regLineBreak.match(line)
+        reportStart = regReportStart.match(line)
+        if reportStart:
+            ifReportStart = True
+        elif callStackLine:
+            if ifReportStart and callStackLine.group(2) != "mythread_wrapper":
+                fout.write(callStackLine.group(2) + " " 
+                    + callStackLine.group(3) + "\n")
+        elif lineBreak:
+            if ifReportStart:
+                ifReportStart = False
+    fin.close()
+    fout.close()
+    
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Valgrind output parser')
+    parser.add_argument('--input', type=str, dest="raceReportIn",
+            action="store", default="none", required=True,
+            help="Valgrind raw race report")
+    parser.add_argument('--output', type=str, dest="raceReportOut",
+            action="store", default="none", required=True,
+            help="Parsed race report")
+    args = parser.parse_args()
+    main(args)
