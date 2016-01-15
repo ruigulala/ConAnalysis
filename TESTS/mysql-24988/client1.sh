944cdda586330b387404cf85a87820457ab2e041
Rui Gu <rui.gu3@gmail.com>
2016-01-15 14:33:00 -0500

    Update submodule & minor fix

--- TESTS/mysql-24988/client1.sh
+++ TESTS/mysql-24988/client1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do mysql-install/bin/mysql -u root -e "FLUSH PRIVILEGES;"; done
+while true; do ~/Workspace/ConAnalysis/concurrency-exploits/mysql-24988/mysql-install-clang/bin/mysql -u root -e "FLUSH PRIVILEGES;"; done
--- TESTS/mysql-24988/client2.sh
+++ TESTS/mysql-24988/client2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do echo 'select count(*) from t1;' ; done | mysql-install/bin/mysql -f -u bugtest -h 127.0.0.1 db1 > /dev/null
+while true; do echo 'select count(*) from t1;' ; done | ~/Workspace/ConAnalysis/concurrency-exploits/mysql-24988/mysql-install-clang/bin/mysql -f -u bugtest -h 127.0.0.1 db1 > /dev/null
