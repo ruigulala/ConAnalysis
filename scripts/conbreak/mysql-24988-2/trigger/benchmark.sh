d037fc99eb2418aabb056bce8b547f501f05fc4e
Jason Zhao <zhao.s.jason@gmail.com>
2016-08-09 22:26:34 +0000

    More prep for mysql-24988

--- scripts/conbreak/mysql-24988-2/trigger/benchmark.sh
+++ scripts/conbreak/mysql-24988-2/trigger/benchmark.sh
@@ -4,6 +4,6 @@
 # and the target executable is ready
 
 # Put your benchmarking / bug-triggering input code here...
-
-
-
+sysbench --test=oltp --oltp-table-size=1000000 --oltp-test-mode=complex \
+         --oltp-read-only=off  --num-threads=10 --max-requests=1000 \
+         --mysql-db=dbca --mysql-user=root run &>/dev/null
--- scripts/conbreak/mysql-24988-2/trigger/startup.sh
+++ scripts/conbreak/mysql-24988-2/trigger/startup.sh
@@ -5,4 +5,10 @@
 # start up proceedure
 
 # Start up code goes here
-./$CONANAL_ROOT/concurrency_exploits/mysql-24988/mysql-install/bin/mysqld --user=root &
+./$CONANAL_ROOT/concurrency_exploits/mysql-24988/mysql-install/bin/mysqld --user=root &>/dev/null &
+
+# Give MySQL a little time to startup
+sleep 3
+
+sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=dbca \
+         --mysql-user=root prepare &>/dev/null
