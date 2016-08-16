6548343ea70a414b1820c944db785baecee5a3a6
Jason Zhao <zhao.s.jason@gmail.com>
2016-08-16 22:27:06 +0000

    Updated readme and cleaned up run.sh

--- TESTS/apache-21287/README.md
+++ TESTS/apache-21287/README.md
@@ -1,1 +1,2 @@
-Thread sanitizer report is located at apache-install/logs/error_log
+# Run Guide
+To generate TSAN reports for Apache-21287, simply type `./run.sh no_static_analysis`.  TSAN reports will be outputted to the `output/` directory.  Please ensure Apache-21287 is built before running our script.
--- TESTS/apache-21287/run.sh
+++ TESTS/apache-21287/run.sh
@@ -52,27 +52,16 @@ then
         echo "You can use mk.sh to automatically build it."
     fi
 
-    #valgrind --version
-    #if [ $? -ne 0 ]
-    #then
-    #    echo "Error: Please install valgrind before running this script."
-    #    echo "We strongly recommend using valgrind 3.11."
-    #fi
     cd $CONANAL_ROOT/TESTS/apache-21287
-    # Start valgrind here!
-    #valgrind --tool=helgrind --trace-children=yes --read-var-info=yes $CONANAL_ROOT/concurrency-exploits/apache-21287/apache-install/bin/apachectl -k start >| valgrind_latest.output 2>&1 &
-    #$CONANAL_ROOT/concurrency-exploits/apache-21287/apache-install/bin/apachectl -k start
     
-	# TSAN flags (set environment variable before running this script)
-    # Just run TSAN (assuming apache server is already compiled with TSAN & clang)
+	# Start apache w/ tsan output 
     env TSAN_OPTIONS="log_path=$CONANAL_ROOT/TESTS/apache-21287/output/tsan" $CONANAL_ROOT/concurrency-exploits/apache-21287/apache-install/bin/apachectl -k start
 
-	# Bug triggering input here!
-    #httperf --server=127.0.1.1 --port=7000 --uri=/pippo.php?variable=1111 --num-conns=10 --num-calls=10
-	#ab -n 1000 -c 100 127.0.1.1:7000/pippo.php?variable=88
+	# Start benchmark
+	ab -n 1000 -c 100 127.0.1.1:7000/pippo.php?variable=88
 fi
 
-if [ "$1" != "no_static_analysis" -a 0 -eq 1 ]
+if [ "$1" != "no_static_analysis" ]
 then
     # Step 3: We'll run our LLVM static analysis pass to analyze the race
     # Use inotify to monitor any new files within the folder
